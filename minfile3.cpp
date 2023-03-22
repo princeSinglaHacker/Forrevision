94a900dc2b4fbdbd95c7476eafed37a6f9b68fca
Prince <singlaprince349@gmail.com>
2023-03-22 09:40:00 -0300

    first commit

---
+++ minfile3.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int first=100;
+	int second=20;
+	int sum=first-second;
+	printf("%d",sum);
+}
---
+++ multfile2.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int first=10;
+	int second=20;
+	int sum=first*second;
+	printf("%d",sum);
+}
